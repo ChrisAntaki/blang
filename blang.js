d48153e15c2dc984e410a7f5c073fe7844fa7791
Chris Antaki <ChrisAntaki@gmail.com>
2015-06-01 15:22:35 -0700

    Created Blang

---
+++ blang.js
@@ -0,0 +1,12 @@
+function $ (query, callback) {
+    var elements = document.querySelectorAll(query);
+    
+    if (!callback) {
+        return Array.prototype.slice.apply(elements);
+    }
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        callback.apply(element, [element, i]);
+    }
+}
